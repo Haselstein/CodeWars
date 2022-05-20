fba242bb39f951be49e36762179b4155e3fb7548
Mikhail Verkhorubov <verhorubov.mihail@gmail.com>
2022-05-20 17:38:22 +0700

    added new solutions

--- src/CodeWars.java
+++ src/CodeWars.java
@@ -7,6 +7,9 @@ public class CodeWars {
 
         //System.out.println("persistence = " + persistence(441888));
 
+        System.out.println(digital_root(347149));
+
+
         
     }
     
@@ -67,8 +70,7 @@ public class CodeWars {
           n = result;
           result = 1;
           answer++;
-          while (n > 0) 
-          {
+          while (n > 0) {
             result *= n%10;
             n /= 10;
           }
@@ -76,4 +78,20 @@ public class CodeWars {
         return answer;
     }
 
+    public static int digital_root(int n) {
+        //Цифровой корень
+        //Находит однозначное число по сумме исходных значений
+        //Пример: 982 -> 9+8+2 = 19 -> 1+9 = 10 -> 1+0 = 1
+        if (n < 10)
+            return n;
+
+        int sum = 0;
+        while (n >= 10) {
+            sum += n%10;
+            n /= 10;
+        }
+        sum += n;
+        n = sum;
+        return digital_root(n);
+    }
 }
